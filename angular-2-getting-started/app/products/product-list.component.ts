ef6ce50abc0a2798c343fb16857aca73f269b808
tomaleksp <tomaleksp@poczta.fm>
2017-03-01 14:32:38 +0000

    added OnInit to the component

--- angular-2-getting-started/app/products/product-list.component.ts
+++ angular-2-getting-started/app/products/product-list.component.ts
@@ -1,17 +1,18 @@
 import { IProduct } from './product';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
     styleUrls: ['app/products/product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
+    
     products: IProduct[] = [
         {
             "productId": 1,
@@ -37,5 +38,9 @@ export class ProductListComponent {
 
     toggleImage(): void{
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void{
+        console.log('In OnInit');
     }
 }
