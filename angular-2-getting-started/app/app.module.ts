82b519aa3ce91e80fd1ab5e5b1bce2c2eb7a9cf9
tomaleksp <tomaleksp@poczta.fm>
2017-03-03 09:27:59 +0000

    add product service and subscribe to it

--- angular-2-getting-started/app/app.module.ts
+++ angular-2-getting-started/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 import { ProductFilterPipe } from './products/product-list.pipe';
 import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star.component';
@@ -10,7 +11,8 @@ import { AppComponent }  from './app.component';
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpModule
     ],
   declarations: [
     AppComponent,
--- angular-2-getting-started/app/products/product-list.component.ts
+++ angular-2-getting-started/app/products/product-list.component.ts
@@ -15,6 +15,7 @@ export class ProductListComponent implements OnInit{
     showImage: boolean = false;
     listFilter: string;
     products: IProduct[];
+    errorMessage: string;
     
     constructor(private _productService: ProductService){
     }
@@ -24,10 +25,12 @@ export class ProductListComponent implements OnInit{
     }
 
     ngOnInit(): void{
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(products => this.products = products,
+                        error => this.errorMessage = <any>error);
     }
 
     onRatingClicked(message: string): void{
-        this.pageTitle = 'Product List: ' + message; 
+        this.pageTitle = 'Product List: ' + message;
     }
 }
