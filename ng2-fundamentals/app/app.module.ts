5ede9fbb382319d6c34d759a08fe2c437d41841e
tomaleksp <tomaleksp@poczta.fm>
2017-04-07 15:24:33 +0100

    clean imports using barrels (index files)

--- ng2-fundamentals/app/app.module.ts
+++ ng2-fundamentals/app/app.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from "@angular/router";
 
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventListResolver,
+    EventRouteActivator
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from "./nav/navbar.component";
-import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
-import { EventDetailsComponent } from "./events/event-details/index";
 import { appRoutes } from './routes';
-import { EventListResolver, EventRouteActivator } from "./events/index";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
 
 @NgModule({
