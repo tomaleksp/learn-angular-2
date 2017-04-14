32792b6a52c26886ca40f06101da9600fdcf43ea
tomaleksp <tomaleksp@poczta.fm>
2017-04-14 10:37:15 +0100

    added output paramaters

--- ng2-fundamentals/app/events/event-details/create-session.component.ts
+++ ng2-fundamentals/app/events/event-details/create-session.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession, restrictedWords } from '../shared/index'
 
 @Component({
+  selector: 'create-session',
   templateUrl: 'app/events/event-details/create-session.component.html',
   styles: [`
     em {float:right; color:#E05C65; padding-left:10px;}
@@ -14,6 +15,8 @@ import { ISession, restrictedWords } from '../shared/index'
   `]
 })
 export class CreateSessionComponent implements OnInit {
+  @Output() saveNewSession = new EventEmitter()
+  @Output() cancelAddSession = new EventEmitter()
   newSessionForm: FormGroup
   name: FormControl
   presenter: FormControl
@@ -39,7 +42,7 @@ export class CreateSessionComponent implements OnInit {
   }
 
   saveSession(formValues) {
-    let session:ISession = {
+    let session: ISession = {
       id: undefined,
       name: formValues.name,
       duration: +formValues.duration,
@@ -48,6 +51,10 @@ export class CreateSessionComponent implements OnInit {
       abstract: formValues.abstract,
       voters: []
     }
-    console.log(session)
+    this.saveNewSession.emit(session)
+  }
+
+  cancel() {
+    this.cancelAddSession.emit()
   }
 }
