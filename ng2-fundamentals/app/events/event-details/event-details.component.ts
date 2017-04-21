b54421b50d68a3b3fd7b646ef20e4b326b808e59
tomaleksp <tomaleksp@poczta.fm>
2017-04-21 09:52:27 +0100

    filter data

--- ng2-fundamentals/app/events/event-details/event-details.component.ts
+++ ng2-fundamentals/app/events/event-details/event-details.component.ts
@@ -15,7 +15,7 @@ export class EventDetailsComponent {
   event: IEvent
   addMode: boolean
   filterBy: string = 'all';
-  //sortBy: string = 'votes';
+  sortBy: string = 'votes';
 
   constructor(private eventService: EventService, private route: ActivatedRoute) {
   }
