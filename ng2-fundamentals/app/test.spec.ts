60ffc5b5cf4a9dfd680655667d0a7709daa023fc
tomaleksp <tomaleksp@poczta.fm>
2017-04-24 10:30:03 +0100

    first test

---
+++ ng2-fundamentals/app/test.spec.ts
@@ -0,0 +1,9 @@
+describe('first test', () => {
+    it('should be true', () => {
+        expect(true).toBe(true);
+    })
+    
+    it('should be false', () => {
+        expect(true).toBe(false);
+    })
+})
