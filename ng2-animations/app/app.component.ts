4dd389c73f44a6cfe8a22a3cf320b0ecfa443694
tomaleksp <tomaleksp@poczta.fm>
2017-04-25 13:01:17 +0100

    first animation

--- ng2-animations/app/app.component.ts
+++ ng2-animations/app/app.component.ts
@@ -1,7 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style, transition, animate, keyframes } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular</h1>`
+    template: `<button [@myTrigger]='state' (click)='toggleState()'>My Button</button>`,
+    styles: [],
+    animations: [
+        trigger('myTrigger',[
+            state('small', style({
+                transform: 'scale(1)'
+            })),
+            state('large', style({
+                transform: 'scale(1.4)'
+            })),
+            // one way
+            // transition('small => large', animate('500ms')),
+            // transition('large => small', animate('500ms')),
+
+            // second way
+            // transition('small => large, large => small', animate('500ms'))
+
+            // third way
+            transition('small <=> large', animate('500ms'))
+        ])
+    ]
 })
-export class AppComponent { }
+export class AppComponent {
+    state: string = 'small';
+
+    toggleState(){
+        this.state = (this.state === 'small' ? 'large' : 'small');
+    }
+ }
