c2061ade35b23eb19d8b29e35ed46f3ce18b5b82
tomaleksp <tomaleksp@poczta.fm>
2017-04-25 13:21:28 +0100

    void animation

--- ng2-animations/app/app.component.ts
+++ ng2-animations/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
 
 @Component({
     selector: 'my-app',
-    template: `<button [@myTrigger]='state' (click)='toggleState()'>My Button</button>`,
+    template: `<button (click)='toggleState()'>My Button</button>
+                <ul>
+                    <li *ngFor="let item of items" [@myTrigger]='state'>{{ item }}</li>
+                </ul>
+    `,
     styles: [],
     animations: [
         trigger('myTrigger',[
@@ -12,6 +16,16 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
             state('large', style({
                 transform: 'scale(1.4)'
             })),
+            state('extra-large', style({
+                transform: 'scale(2)'
+            })),
+            state('fadeIn', style({
+                opacity: '1'
+            })),
+            transition('void => *', [
+                style({ opacity: '0', transform: 'translateY(20px)' }),
+                animate('500ms')
+            ])
             // one way
             // transition('small => large', animate('500ms')),
             // transition('large => small', animate('500ms')),
@@ -20,14 +34,22 @@ import { Component, trigger, state, style, transition, animate, keyframes } from
             // transition('small => large, large => small', animate('500ms'))
 
             // third way
-            transition('small <=> large', animate('500ms'))
+            // transition('small <=> large', animate('500ms'))
+
+            // transition('* => small', animate('500ms'))
+            // transition('* => *', animate('500ms'))
+
+
         ])
     ]
 })
 export class AppComponent {
-    state: string = 'small';
+    state: string = 'fadeIn';
+    items: any[] = ['item1','item2','item2','item3'];
 
     toggleState(){
-        this.state = (this.state === 'small' ? 'large' : 'small');
+        //this.state = (this.state === 'small' ? 'large' : 'small');
+        this.items.push('another item');
+        this.state = 'fadeIn';
     }
  }
